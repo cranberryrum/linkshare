2fe6398d05d130e191a9262736ddc2b628841aac
cranberryrum <adityakoltedes@gmail.com>
2026-04-26 11:54:46 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import posthog from 'posthog-js'
 import App from './App.tsx'
 import './index.css'
+
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+  person_profiles: 'identified_only',
+  capture_pageview: true,
+  capture_pageleave: true,
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
